52d701c41242902157b1c0946c00bb53617786d2
Andrew Zola <and.zola.77@gmail.com>
2022-11-13 14:04:35 +0100

    Commit with count

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String file = "C:\\Users\\zolaa\\Test\\Test1.txt";
         FileInputStream fis = new FileInputStream(file);
-
+        int c = 0;
         int i;
         while ((i = fis.read()) != -1) {
-            System.out.print((char) i);
+            if (c >= '0' && c <= '9') ;
+            c++;
         }
+        System.out.println(c);
         fis.close();
-        System.out.println();
     }
 }
 
